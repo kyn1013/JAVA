81292a743a3bc8a85f0b93cd4216712359af32b2
kyn1013 <yn112511@naver.com>
2022-01-05 02:16:01 +0900

    java

---
+++ StringApp.java
@@ -0,0 +1,18 @@
+
+public class StringApp {
+
+	public static void main(String[] args) {
+		//Character vs String
+		System.out.println("Hello World"); //String
+		System.out.println('H'); //Character
+		System.out.println("H"); //String
+		
+		//New line
+		System.out.println("Hello \nWorld");
+		
+		//escape
+		System.out.println("Hello \"World\""); //Hello \"World\"
+
+	}
+
+}
