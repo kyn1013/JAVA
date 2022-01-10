e3650d6dfa7cc1cda3f2af028786841039dd1beb
kyn1013 <yn112511@naver.com>
2022-01-10 22:22:52 +0900

    Create BooleanApp.java

---
+++ BooleanApp.java
@@ -0,0 +1,17 @@
+
+public class BooleanApp {
+
+	public static void main(String[] args) {
+		
+		System.out.println(true);
+		System.out.println(false);
+		
+		String foo="Hello World";
+		//String true = "Hello World"; reserved word
+		
+		System.out.println(foo.contains("World"));
+		System.out.println(foo.contains("egoing"));
+
+	}
+
+}
